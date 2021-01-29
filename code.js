31f7e4559eac121020c1831a5993a9ea659bfa1a
hanbokhe <64712768+hanbokhe@users.noreply.github.com>
2021-01-29 06:22:31 -0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,16 @@
+  var sameLength = actual.length === expected.length;
+  var sameValue = true;
+
+  for (var i = 0; i < expected.length; i++)
+  {
+      if (actual[i]!== expected[i])
+      {
+          sameValue = false;
+          break;
+      }
+  }
+  if (sameValue && sameLength){
+      console.log('passed');
+  }else{
+    console.log("FAILED ["+ testName +"] Expected \"" + expected + "\", but got \"" + actual + "\"");
+  }
